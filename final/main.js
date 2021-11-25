7b39b53abf009a9fd28df7cdabf16b690674d263
That Random <andrewwoan@gmail.com>
2021-11-25 16:21:47 -0500

    First Commit

---
+++ final/main.js
@@ -0,0 +1,25 @@
+const menuBtn = document.querySelector(".menu-div");
+const exitBtn = document.querySelector(".exit");
+
+let t1 = gsap.timeline({ paused: true });
+t1.to(".menu", { opacity: 1, duration: 1, top: 0, ease: Power2.easeInOut });
+t1.to(
+    ".nav",
+    {
+        opacity: 1,
+        marginBottom: 0,
+        duration: 1,
+        ease: Power2.easeInOut,
+        stagger: 0.3,
+    },
+    ">-0.5"
+);
+
+menuBtn.addEventListener("click", () => {
+    t1.play().timeScale(1);
+});
+
+exitBtn.addEventListener("click", () => {
+    t1.timeScale(2.5);
+    t1.reverse();
+});
